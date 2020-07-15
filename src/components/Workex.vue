4c0866ce8cc83256768d5fc5bf8a04f2eebfb93f
Gilchrist32 <32stephencurry@gmail.com>
2020-07-15 22:54:12 +0800

    Added Workex to About

---
+++ src/components/Workex.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="work">
+        <b-col cols="sm-auto" class="mt-0 mb-2 mx-2" >
+        <b-card id="card-about" class="overflow-hidden" style="max-width: 100%;">
+          <b-row>
+            <b-col sm="12">
+              <b-card-text id="card-text">Work Experience</b-card-text>
+              <hr>
+              <b-form-group class="label" label="IT Specialist">
+                <b-form-input class="text-input" value="Department of Labor and Employment Region 7" disabled></b-form-input>
+                <span id="address">Insular Bldg., General Maxilom Gorordo Avenue, Cebu City</span>
+              </b-form-group>     
+              <b-form-group  class="label" label="OJT IT Specialist">
+                <b-form-input class="text-input" value="Department of Labor and Employment Region 7" disabled></b-form-input>
+                <span id="address">Insular Bldg., General Maxilom Gorordo Avenue, Cebu City</span>
+              </b-form-group>           
+            </b-col>
+          </b-row>
+        </b-card>
+        </b-col>
+    </div>
+</template>
