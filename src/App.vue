57361c6685695ee140b6024600fad275d5fcb88e
Gilchrist32 <32stephencurry@gmail.com>
2020-07-15 20:33:41 +0800

    Added Navabr

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,34 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <Navbar />
+      <vue-page-transition name="fade-in-right" class="content">
+
+      <router-view />  <!-- DYNAMIC CONTENT LOADED -->
+
+    </vue-page-transition>
     </div>
     <router-view/>
   </div>
 </template>
+<script>
+export default {
+  components: {
+    Navbar: () => import('@/components/Navbar'),
+  }
+}
+</script>
 
 <style lang="scss">
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+}
+
+   hr{
+  background: #ffff;
 }
 
 #nav {
@@ -25,8 +39,12 @@
     color: #2c3e50;
 
     &.router-link-exact-active {
-      color: #42b983;
+      color: #ffffff;
     }
   }
+}
+.bg{
+   /* The image used */
+    background-color: #2c3e50;
 }
 </style>
