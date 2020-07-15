87ff5fdfb4ea894090e24b17f9bd77993fc7505a
Gilchrist32 <32stephencurry@gmail.com>
2020-07-15 19:22:59 +0800

    UPDATED

--- src/main.js
+++ src/main.js
@@ -2,8 +2,48 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import 'bootstrap-vue/dist/bootstrap-vue-icons.min.css'
+import Vuelidate from 'vuelidate'
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { faFontAwesome } from '@fortawesome/free-brands-svg-icons';
+import { faSpinner, faAlignLeft } from '@fortawesome/free-solid-svg-icons';
+import { faFacebook } from '@fortawesome/free-brands-svg-icons';
+import { faTwitter } from '@fortawesome/free-brands-svg-icons';
+import { faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import VueTimeago from 'vue-timeago';
+import VuePageTransition from 'vue-page-transition';
+import BackToTop from 'vue-backtotop';
 
+library.add(faUserSecret)
+library.add(faFontAwesome)
+library.add(faSpinner, faAlignLeft)
+library.add(faFacebook)
+library.add(faTwitter)
+library.add(faGithub)
+library.add(faInstagram)
+
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 Vue.config.productionTip = false
+
+Vue.use(Vuelidate)
+Vue.use(BootstrapVue);
+Vue.use(BootstrapVueIcons)
+Vue.use(VuePageTransition)
+Vue.use(BackToTop)
+
+Vue.use(VueTimeago, {
+  locale: 'en',
+  locales: {
+    'zh-CN': require('date-fns/locale/zh_cn')
+  }
+})
 
 new Vue({
   router,
