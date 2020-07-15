d96a0d61741883aa523cdf590f550e97ff5faa61
Gilchrist32 <32stephencurry@gmail.com>
2020-07-15 21:53:22 +0800

    Added education, project

--- src/router/index.js
+++ src/router/index.js
@@ -5,25 +5,32 @@ import Home from '../views/Home.vue'
 Vue.use(VueRouter)
 
   const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
-
-const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
-})
-
-export default router
+    {
+      path: '/',
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/about',
+      name: 'About',
+      component: () => import('../views/About.vue')
+    },
+    {
+      path: '/education',
+      name: 'Education',
+      component: () => import('../views/Education.vue')
+    },
+    {
+      path: '/project',
+      name: 'Project',
+      component: () => import('../views/Project.vue')
+    }
+  ]
+  
+  const router = new VueRouter({
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes
+  })
+  
+  export default router
